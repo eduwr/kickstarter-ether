9d0c1243e8ca1d8beeaafbe27df8db85dad6c925
eduwr <eduwric@gmail.com>
2021-07-20 22:35:01 -0300

    feat: campaigns list

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "semantic-ui-css/semantic.min.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../../ethereum/factory";
 
 const CampaignIndex = ({ campaigns }) => {
-  return <div>{campaigns[0]}</div>;
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  };
+
+  return (
+    <div>
+      <h3>Open Campaigns</h3>
+      {renderCampaigns()}
+      <Button content="Create Campaign" icon="add circle" primary />
+    </div>
+  );
 };
 
 export default CampaignIndex;
