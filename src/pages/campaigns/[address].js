d9031f210ce4876cbb679730afc09c5db6dac2ae
eduwr <eduwric@gmail.com>
2021-07-21 01:13:28 -0300

    feat: fetch campaign summary

--- src/pages/campaigns/[address].js
+++ src/pages/campaigns/[address].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Campaign from "../../../ethereum/campaign";
 
 const Show = () => {
   const router = useRouter();
@@ -8,3 +9,21 @@ const Show = () => {
 };
 
 export default Show;
+
+export async function getServerSideProps({ params }) {
+  console.log(params);
+  const campaign = Campaign(params.address);
+  const summary = await campaign.methods.getSummary().call();
+
+  return {
+    props: {
+      summary: {
+        minimumContribution: summary[0],
+        balance: summary[1],
+        requestsCount: summary[2],
+        approversCount: summary[3],
+        manager: summary[4],
+      },
+    },
+  };
+}
--- src/pages/newcampaign.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default () => {
-  return <h1>This is new campaign page</h1>;
-};
